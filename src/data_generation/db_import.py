d2d4f2c8b083f3ec2f42c9cbd2d23f669e830625
Yevhenia434 <rusyn.yevheniia@student.uzhnu.edu.ua>
2026-06-04 11:51:37 +0300

    Update db_import.py

--- src/data_generation/db_import.py
+++ src/data_generation/db_import.py
@@ -3,18 +3,15 @@ import csv
 import os
 
 def import_csv_to_sqlite():
-    # МАГІЧНИЙ РЯДОК: Дізнаємося точну папку, де лежить цей скрипт
     current_folder = os.path.dirname(os.path.abspath(__file__))
     
-    # Змінюємо робочу папку програми на ту, де лежить наш скрипт
     os.chdir(current_folder)
     print(f"Robocha papka zminena na: {current_folder}")
-    
-    # 1. Підключаємося до SQLite (файл бази даних створиться поруч)
+  
     conn = sqlite3.connect("storepulse.db")
     cursor = conn.cursor()
     
-    # Список наших таблиць для імпорту
+
     tables = ["customers", "products", "orders", "order_items"]
     
     print("\n--- Start importu danykh v SQL ---")
@@ -26,17 +23,17 @@ def import_csv_to_sqlite():
             print(f"Error: File {csv_name} ne znaydeno в папці!")
             continue
             
-        # Читаємо CSV файл
+    
         with open(csv_name, mode="r", encoding="utf-8") as f:
             reader = csv.reader(f)
-            headers = next(reader)  # Беремо заголовки
+            headers = next(reader)  
             
-            # Створюємо таблицю в SQL
+        
             columns_str = ", ".join([f"{h} TEXT" for h in headers])
             cursor.execute(f"DROP TABLE IF EXISTS {table}")
             cursor.execute(f"CREATE TABLE {table} ({columns_str})")
             
-            # Заливаємо рядки
+    
             placeholders = ", ".join(["?"] * len(headers))
             rows = [row for row in reader]
             cursor.executemany(f"INSERT INTO {table} VALUES ({placeholders})", rows)
